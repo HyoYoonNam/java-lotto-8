168c5ae00a3c176a90e5a90e89d0531be8c05ed0
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-31 21:17:11 +0900

    feat(view): ✨ 보너스 번호를 입력받는다
    readLineAsInt가 내부적으로 parseToInteger를 호출하여 타입 변환을
    시도하도록 한다.
    
    따라서 이전에는 parseToInteger가 뱉는 예외 메시지가 "당첨 번호가
    ..."처럼 '당첨 번호를 입력 받는 기능'에만 한정적이었다면, 이제는 "입력된
    값이 ..."처럼 일반적인 상황에 대응할 수 있다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,7 +13,8 @@ import java.util.stream.Stream;
 public final class InputView {
     private static final String READ_WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_DELIMITER = ",";
-    private static final String WINNING_NUMBER_IS_NOT_INTEGER = "입력된 당첨 번호가 숫자가 아닙니다: %s";
+    private static final String INPUT_IS_NOT_INTEGER = "입력된 값이 숫자가 아닙니다: %s";
+    private static final String READ_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
     /**
      * 콤마(,)로 구분된 당첨 번호 목록을 입력받고, 콤마(,) 기준으로 분리하여 리턴한다.
@@ -30,6 +31,16 @@ public final class InputView {
                 .toList();
     }
 
+    /**
+     * 입력받은 보너스 번호를 리턴한다.
+     *
+     * @throws IllegalArgumentException 입력이 숫자가 아니라면 발생한다.
+     */
+    public static int readBonusNumber() {
+        System.out.println(READ_BONUS_NUMBER_PROMPT);
+        return readLineAsInt();
+    }
+
     private static String readLine() {
         return Console.readLine();
     }
@@ -38,7 +49,16 @@ public final class InputView {
         try {
             return Integer.valueOf(s);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(String.format(WINNING_NUMBER_IS_NOT_INTEGER, s), e);
+            throw new IllegalArgumentException(String.format(INPUT_IS_NOT_INTEGER, s), e);
+        }
+    }
+
+    private static int readLineAsInt() {
+        String line = Console.readLine();
+        try {
+            return parseToInteger(line);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(String.format(INPUT_IS_NOT_INTEGER, line), e);
         }
     }
 }
