09477d7233fa172247538b007ddcb5324659dd7e
HyoYoonNam <dev.rudevico@gmail.com>
2025-10-31 22:35:11 +0900

    refactor(view): ♻️ 6개의 당첨 번호에 대한 용어 변경
    용어를 winning numbers에서 regular numbers로 변경한다.
    
    6개의 당첨 번호와 1개의 보너스 번호를 포함해서 표현할 용어가 필요한데,
    마땅치가 않다.
    
    네이버에 '로또 당첨 번호'라고 검색하면 6개의 번호와 보너스 번호 1개를
    함께 표시해주는 것을 근거로 winning numbers는 이 7개를 한번에 일컫는
    용어로 정한다.
    
    기존의 6개를 일컫는 용어는 regular numbers로 한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -12,7 +12,7 @@ import lotto.constant.exception.ErrorMessage;
  */
 
 public final class InputView {
-    private static final String READ_WINNING_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
+    private static final String READ_REGULAR_NUMBERS_PROMPT = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_DELIMITER = ",";
     private static final String INPUT_IS_NOT_INTEGER = ErrorMessage.MESSAGE_PREFIX + "입력된 값이 숫자가 아닙니다: %s";
     private static final String READ_BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
@@ -23,8 +23,8 @@ public final class InputView {
      * @return 당첨 번호들
      * @throws IllegalArgumentException 입력에 콤마(,)를 제외하고, 숫자가 아닌 요소가 있다면 발생한다.
      */
-    public static List<Integer> readWinningNumbers() {
-        System.out.println(READ_WINNING_NUMBERS_PROMPT);
+    public static List<Integer> readRegularNumbers() {
+        System.out.println(READ_REGULAR_NUMBERS_PROMPT);
         String[] split = readLine().split(INPUT_DELIMITER);
 
         return Stream.of(split)
