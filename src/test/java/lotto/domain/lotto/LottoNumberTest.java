5ffc2ea447f09216abdf3eaa94152f693c383272
HyoYoonNam <dev.rudevico@gmail.com>
2025-11-03 22:26:25 +0900

    style(test): 🎨 @DisplayName 애노테이션 순서 위로 조정

--- src/test/java/lotto/domain/lotto/LottoNumberTest.java
+++ src/test/java/lotto/domain/lotto/LottoNumberTest.java
@@ -9,32 +9,32 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoNumberTest {
+    @DisplayName("로또 번호를 생성한다.")
     @ParameterizedTest(name = "[{index}] {0} -> 로또 번호 생성 성공")
     @ValueSource(ints = {
             1, 45,          // 경계값
             12, 22, 32, 42  // 일반값
     })
-    @DisplayName("로또 번호를 생성한다.")
     void valueOf_createLottoNumber_numberInrange(int inRangednumber) {
         LottoNumber lottoNumber = LottoNumber.valueOf(inRangednumber);
 
         assertThat(lottoNumber.getNumber()).isEqualTo(inRangednumber);
     }
 
+    @DisplayName("숫자 범위를 벗어나는 로또 번호의 생성 시도에 대해서 예외를 발생시킨다.")
     @ParameterizedTest(name = "[{index}] {0} -> 예외 발생")
     @ValueSource(ints = {
             0, 46,  // 경계값
             -1      // 음수
     })
-    @DisplayName("숫자 범위를 벗어나는 로또 번호의 생성 시도에 대해서 예외를 발생시킨다.")
     void valueOf_throwsException_numberOutOfRange(int outOfRangedNumber) {
         assertThatThrownBy(() -> LottoNumber.valueOf(outOfRangedNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContainingAll("[ERROR]", "범위", "1~45");
     }
 
-    @Test
     @DisplayName("로또 번호가 같으면 동일한 객체로 판단한다")
+    @Test
     void equals_returnTrue_numbersAreSame() {
         LottoNumber lottoNumberA = LottoNumber.valueOf(1);
         LottoNumber lottoNumberB = LottoNumber.valueOf(1);
--- src/test/java/lotto/domain/lotto/LottoVendingMachineTest.java
+++ src/test/java/lotto/domain/lotto/LottoVendingMachineTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class LottoVendingMachineTest {
     private static final int LOTTO_PRICE = 1000;
 
-    @Test
     @DisplayName("구입 금액에 해당하는 만큼 로또를 발행한다.")
+    @Test
     void purchase_ReturnLottosAsPurchaseAmount_validPurchaseAmount() {
         int purchaseAmount = 1000;
         int expectedLottoCount = purchaseAmount / LOTTO_PRICE;
@@ -23,12 +23,12 @@ public class LottoVendingMachineTest {
         assertThat(lottos).hasSize(expectedLottoCount);
     }
 
+    @DisplayName("구입 금액이 1,000원으로 나누어 떨어지지 않는다면 예외를 발생한다")
     @ParameterizedTest(name = "[{index}] {0} -> 예외 발생")
     @ValueSource(ints = {
             999, 1001,  // 경계값
             1200        // 일반값
     })
-    @DisplayName("구입 금액이 1,000원으로 나누어 떨어지지 않는다면 예외를 발생한다")
     void purchase_throwsException_invalidPurchaseAmount(int invalidPurchaseAmount) {
         assertThatThrownBy(() -> LottoVendingMachine.purchase(invalidPurchaseAmount))
                 .isInstanceOf(IllegalArgumentException.class)
