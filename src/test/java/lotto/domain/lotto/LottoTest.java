ba097f6751cf5681e5eb455f40ea09f72b50cb79
HyoYoonNam <dev.rudevico@gmail.com>
2025-11-03 23:05:47 +0900

    test: ✅ Lotto의 누락된 테스트 추가

--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -35,4 +35,42 @@ class LottoTest {
             assertThat(lotto.getNumbers()).isSorted();
         }
     }
+
+    @Nested
+    @DisplayName("로또 번호의 포함 여부(contains) 테스트")
+    class ContainsTest {
+        @DisplayName("로또가 번호를 포함하면 true를 리턴한다.")
+        @Test
+        void contains_returnTrue_lottoContainsNumber() {
+            int containedNumber = 1;
+            Lotto lotto = Lotto.from(List.of(containedNumber, 2, 3, 4, 5, 6));
+            LottoNumber lottoNumber = LottoNumber.valueOf(containedNumber);
+
+            assertThat(lotto.contains(lottoNumber)).isTrue();
+        }
+
+        @DisplayName("로또가 번호를 포함하지 않으면 false를 리턴한다.")
+        @Test
+        void contains_returnFalse_lottoNotContainsNumber() {
+            Lotto lotto = Lotto.from(List.of(2, 3, 4, 5, 6, 7));
+            LottoNumber lottoNumber = LottoNumber.valueOf(1);
+
+            assertThat(lotto.contains(lottoNumber)).isFalse();
+        }
+    }
+
+    @Nested
+    @DisplayName("로또가 가지는 번호들(getNumbers) 테스트")
+    class GetNumbers {
+        @DisplayName("로또가 가진 번호가 정렬되어 리턴된다.")
+        @Test
+        void getNumbers_returnSortedNumbers_lottoFromUnsortedNumbers() {
+            List<Integer> unsortedNumbers = List.of(1, 5, 2, 4, 6, 3);
+            Lotto lotto = Lotto.from(unsortedNumbers);
+
+            List<Integer> numbers = lotto.getNumbers();
+
+            assertThat(numbers).containsExactly(1, 2, 3, 4, 5, 6);
+        }
+    }
 }
